67a5c8857f65471bd57bf8b96de0b243b895a9e1
Yago Azevedo <yago.dev@gmail.com>
2024-04-01 21:56:13 -0300

    código aula 9

--- Aula 7/imagem-do-dia/src/App.jsx
+++ Aula 7/imagem-do-dia/src/App.jsx
@@ -6,15 +6,23 @@ import moment from 'moment'
 function App() {
   const [enderecoImagem, setEnderecoImagem] = useState("")
   const [data, setData] = useState("")
+  const [carregando, setCarregando] = useState(true)
 
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2024-01-01'
-  const buscarDadosApi = () => {
-    axios.get(url).then((dadosNASA) => {
-      console.log(dadosNASA.data)
-      setEnderecoImagem(dadosNASA.data.url)
-      const dataDaRequisicao = moment(new Date(dadosNASA.data.date)).format("DD/MM/YYYY")
-      setData(dataDaRequisicao)
-    })
+  const buscarDadosApi = async () => {
+    setCarregando(true)
+    const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${data}`
+    // axios.get(url).then(dadosNASA => {
+    //   console.log(dadosNASA.data)
+    //   setEnderecoImagem(dadosNASA.data.url)
+    //   const dataDaRequisicao = moment(new Date(dadosNASA.data.date)).format("DD/MM/YYYY")
+    //   setData(dataDaRequisicao)
+    // })
+    const dadosNASA = await axios.get(url)
+    console.log(dadosNASA.data)
+    setEnderecoImagem(dadosNASA.data.url)
+    const dataDaRequisicao = moment(new Date(dadosNASA.data.date)).format("DD/MM/YYYY")
+    setData(dataDaRequisicao)
+    setCarregando(false)
   }
 
   useEffect(() => {
@@ -23,11 +31,24 @@ function App() {
 
   return (
     <>
-      <h1>Imagem do dia {data}</h1>
-      <div style={{width: '100%'}}>
-        <img src={enderecoImagem} width={500} />
-      </div>
-      <button onClick={buscarDadosApi}>Buscar dados da API</button>
+      {
+        carregando ? <h1>Carregando...</h1> : (
+          <>
+            <h1>Imagem do dia {data}</h1>
+            <div style={{width: '100%', marginBottom: '10px'}}>
+              <img src={enderecoImagem} width={500} />
+            </div>
+            <input 
+              style={{marginRight: '10px'}} 
+              type='text' 
+              value={data} 
+              onChange={(event) => { setData(event.target.value) }}
+            />
+            <button onClick={buscarDadosApi}>Buscar dados da API</button>
+          </>
+        )
+      }
+      
     </>
   )
 }
